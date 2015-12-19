ecabd37604fda7b104496cffb373f9866706c2cc
Mike Milano <coder1@gmail.com>
2015-12-18 20:47:52 -0800

    dryrun implementation

--- bin/ec2snapshots.js
+++ bin/ec2snapshots.js
@@ -5,12 +5,17 @@ var snapshots = require('../lib/snapshots');
 
 program
   .version('0.0.1')
+  .option('-d, --dryrun', 'Dry run')
   .arguments('<region>]')
-  .action(function (region) {
+  .action(function (region, options) {
+    var dryrun = options.dryrun !== undefined && options.dryrun;
+    if (dryrun) {
+      console.log('** Dry run **');
+    }
     var regions = region.split(',');
-    snapshots.purgeSnapshots(regions, function(err, data) {
+    snapshots.purgeSnapshots(regions, dryrun, function(err, data) {
       if (err) console.error(err, err.stack);
-      snapshots.createSnapshots(regions, function (err, data) {
+      snapshots.createSnapshots(regions, dryrun, function (err, data) {
         if (err) console.error(err, err.stack);
       });
     });
